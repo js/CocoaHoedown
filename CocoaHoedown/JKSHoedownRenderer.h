99d146b46397a0fac777fe77c96eb9e3024b43d3
johsoren <johan.sorensen@finn.no>
2014-01-05 20:45:51 +0100

    Project structure shenenigans

---
+++ CocoaHoedown/JKSHoedownRenderer.h
@@ -0,0 +1,13 @@
+//
+//  JKSHoedownRenderer.h
+//  CocoaHoedown
+//
+//  Created by Sørensen, Johan on 05.01.14.
+//  Copyright (c) 2014 Frosthaus AS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JKSHoedownRenderer : NSObject
+
+@end
---
+++ CocoaHoedown/JKSHoedownRenderer.m
@@ -0,0 +1,13 @@
+//
+//  JKSHoedownRenderer.m
+//  CocoaHoedown
+//
+//  Created by Sørensen, Johan on 05.01.14.
+//  Copyright (c) 2014 Frosthaus AS. All rights reserved.
+//
+
+#import "JKSHoedownRenderer.h"
+
+@implementation JKSHoedownRenderer
+
+@end
